cab36e43e8f14f9d1c672f9ccea6c564b285c4d9
Ansoumane Kande <liam-kande@Ansoumanes-MBP.home>
2017-10-14 06:29:13 -0400

    built the GuestList Component

---
+++ src/App.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react'
+import GuestList from './GuestList'
+import logo from './LKweblogo.svg'
+class App extends Component {
+
+  state = {
+    guests: [
+      {
+        name: 'Apple',
+        isConfirmed: false
+      },
+      {
+        name: 'Liam',
+        isConfirmed: true
+      },
+      {
+        name: 'Liam',
+        isConfirmed: true
+      }
+    ]
+  }
+  getTotalInvited = () => this.state.guests.length
+  // getAttendingGuests = () =>
+  // getUnconfirmedGuests = () =>
+
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <h1>RSVP</h1>
+          <a href="http://liamkande.com/"><img src={logo} width={70} alt="LKPLAYS Logo" />
+          </a>
+          <form>
+              <input type="text" value="Safia" placeholder="Invite Someone" />
+              <button type="submit" name="submit" value="submit">Submit</button>
+          </form>
+        </header>
+        <div className="main">
+          <div>
+            <h2>Invitees</h2>
+            <label>
+              <input type="checkbox" /> Hide those who haven't responded
+            </label>
+          </div>
+          <table className="counter">
+            <tbody>
+              <tr>
+                <td>Attending:</td>
+                <td>2</td>
+              </tr>
+              <tr>
+                <td>Unconfirmed:</td>
+                <td>1</td>
+              </tr>
+              <tr>
+                <td>Total:</td>
+                <td>3</td>
+              </tr>
+            </tbody>
+          </table>
+          <GuestList guests={this.state.guests}/>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App
