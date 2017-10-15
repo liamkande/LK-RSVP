645010a6992e8dcf4386b620d6e1a888cee2c06a
Ansoumane Kande <liam-kande@Ansoumanes-MBP.home>
2017-10-15 06:39:06 -0400

    Created Counter componet

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 
 import logo from './LKweblogo.svg'
 import GuestList from './GuestList'
+import Counter from './Counter'
 
 class App extends Component {
 
@@ -67,8 +68,8 @@ class App extends Component {
       })
     });
 
-  toggleFilter = () =>
-    this.setState({ isFiltered: !this.state.isFiltered })
+toggleFilter = () =>
+  this.setState({ isFiltered: !this.state.isFiltered })
 
 handleNameInput = e =>
   this.setState({pendingGuest: e.target.value})
@@ -89,10 +90,16 @@ newGuestSubmitHandler = e => {
 }
 
   getTotalInvited = () => this.state.guests.length
-  // getAttendingGuests = () =>
+  getAttendingGuests = () =>
+  this.state.guests.reduce((total, guest) => guest.isConfirmed ?
+  total + 1 :
+  total, 0)
   // getUnconfirmedGuests = () =>
 
   render() {
+    const totalInvited = this.getTotalInvited()
+    const numberAttending = this.getAttendingGuests()
+    const numberUnconfirmed = totalInvited - numberAttending
     return (
       <div className="App">
         <header>
@@ -117,23 +124,12 @@ newGuestSubmitHandler = e => {
                 checked={this.state.isFiltered} /> Hide those who haven't responded
             </label>
           </div>
-          <table className="counter">
-            <tbody>
-              <tr>
-                <td>Attending:</td>
-                <td>2</td>
-              </tr>
-              <tr>
-                <td>Unconfirmed:</td>
-                <td>1</td>
-              </tr>
-              <tr>
-                <td>Total:</td>
-                <td>3</td>
-              </tr>
-            </tbody>
-          </table>
 
+          <Counter
+            totalInvited={totalInvited}
+            numberAttending={numberAttending}
+            numberUnconfirmed={numberUnconfirmed}
+            />
           <GuestList
             guests={this.state.guests}
             toggleConfirmationAt={this.toggleConfirmationAt}
